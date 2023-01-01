84196e2fddf670a84489cbe0d8e1d181252327d6
cchanmi <gkfn980@hs.ac.kr>
2023-01-02 04:03:16 +0900

    [Chore] #5 - 컬러 상수화

--- Smeme-iOS/Smeme-iOS/Resources/Extension/UIColor+.swift
+++ Smeme-iOS/Smeme-iOS/Resources/Extension/UIColor+.swift
@@ -5,4 +5,30 @@
 //  Created by 황찬미 on 2022/12/29.
 //
 
-import Foundation
+import UIKit
+
+// MARK: - Color StyleGuide
+
+extension UIColor {
+    
+    class var white: UIColor? { return UIColor(named: "white") }
+    
+    class var gray100: UIColor? { return UIColor(named: "gray100") }
+    
+    class var gray300: UIColor? { return UIColor(named: "gray300") }
+    
+    class var gray400: UIColor? { return UIColor(named: "gray400") }
+    
+    class var gray500: UIColor? { return UIColor(named: "gray500") }
+    
+    class var gray600: UIColor? { return UIColor(named: "gray600") }
+    
+    class var gray700: UIColor? { return UIColor(named: "gray700") }
+    
+    class var primary: UIColor? { return UIColor(named: "primary") }
+    
+    class var secondary: UIColor? { return UIColor(named: "secondary") }
+    
+    class var background: UIColor? { return UIColor(named: "background") }
+    
+}
