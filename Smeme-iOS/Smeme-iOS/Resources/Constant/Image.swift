e06339adf555580837565a1266ba984847385684
cchanmi <gkfn980@hs.ac.kr>
2023-01-04 16:08:08 +0900

    [Feat] #5 - 아이콘 에셋 상수화

---
+++ Smeme-iOS/Smeme-iOS/Resources/Constant/Image.swift
@@ -0,0 +1,65 @@
+//
+//  Image.swift
+//  Smeme-iOS
+//
+//  Created by 황찬미 on 2023/01/04.
+//
+
+import UIKit
+
+extension Constant {
+    struct Image {
+        
+        // MARK: - Login
+        
+        // MARK: - NaviView
+        
+        static let icnProfile = UIImage(named: "icnProfile")!
+        static let icnBack = UIImage(named: "icnBack")!
+        static let icnSetting = UIImage(named: "icnSetting")!
+        static let icnArrowUnder = UIImage(named: "icnArrowUnder")!
+        
+        // MARK: - BottomView
+        
+        static let icnLike = UIImage(named: "icnLike")!
+        static let icnCheckBox = UIImage(named: "icnCheckBox")!
+        static let icnCheckBoxDisabled = UIImage(named: "icnCheckBoxDisabled")!
+        static let icnCheckBoxSelected = UIImage(named: "icnCheckBoxSelected")!
+        static let icnCheckBoxSelectedDisabled = UIImage(named: "icnCheckBoxSelectedDisabled")
+        
+        // MARK: - Tabbar
+        
+        static let tabBarMyDiary = UIImage(named: "tabBarMyDiary")!
+        static let tabBarMyDiarySelected = UIImage(named: "tabBarMyDiarySelected")!
+        static let tabBarOpenDiary = UIImage(named: "tabBarOpenDiary")!
+        static let tabBarOpenDiarySelected = UIImage(named: "tabBarOpenDiarySelected")!
+        static let tabBarStash = UIImage(named: "tabBarStash")!
+        static let tabBarStashSelected = UIImage(named: "tabBarStashSelected")!
+        
+        // MARK: - Main
+        
+        static let icnPageLeft = UIImage(named: "icnPageLeft")!
+        static let icnPageRight = UIImage(named: "icnPageRight")!
+        static let icnPageRightDisabled = UIImage(named: "icnPageRightDisabled")!
+        
+        // MARK: - DiaryPost
+        
+        static let icnRefresh = UIImage(named: "icnRefresh")!
+        
+        // MARK: - BottomSheet
+        
+        static let icnCheckSelected = UIImage(named: "icnCheckSelected")!
+        
+        // MARK: - OpenDiary
+        
+        static let icnLikeSmall = UIImage(named: "icnLikeSmall")!
+        
+        // MARK: - ScrapStash
+        
+        static let icnBrowser = UIImage(named: "icnBrowser")!
+        static let icnCopy = UIImage(named: "icnCopy")!
+        static let icnTranslate = UIImage(named: "icnTranslate")!
+        static let icnDelete = UIImage(named: "icnDelete")!
+        
+    }
+}
