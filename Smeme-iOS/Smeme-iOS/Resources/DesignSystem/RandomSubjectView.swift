cceb8bdafa56a6cd87df091ba16758d69c394e9a
임주민 <limj1213@gmail.com>
2023-01-05 04:54:54 +0900

    [ADD] #13 - 새로고침 버튼 있는 랜덤주제뷰 UI 구현

--- Smeme-iOS/Smeme-iOS/Resources/DesignSystem/RandomSubjectView.swift
+++ Smeme-iOS/Smeme-iOS/Resources/DesignSystem/RandomSubjectView.swift
@@ -9,3 +9,79 @@ import UIKit
 
 import SnapKit
 import Then
+
+struct RandomSubjectViewModel {
+    var contentText: String
+}
+
+final class RandomSubjectView: UIView {
+    
+    // MARK: - Property
+    
+    var refreshSubjectContent: (() -> Void)?
+    
+    // MARK: - UI Property
+    
+    private let questionLabel = UILabel().then {
+        $0.font = .body3
+        $0.text = "Q."
+        $0.textColor = .primary
+    }
+    
+    private let contentLabel = UILabel().then {
+        $0.textAlignment = .left
+        $0.font = .body4
+        $0.textColor = .black
+        $0.numberOfLines = 0
+        $0.lineBreakMode = .byWordWrapping
+    }
+    
+    private let refreshButton = UIButton().then {
+        $0.setImage(Constant.Image.icnRefresh, for: .normal)
+    }
+    
+    // MARK: - @objc
+    
+    // MARK: - Custom Method
+    
+    private func setAttribute() {
+        addSubviews([questionLabel, contentLabel, refreshButton])
+        
+        backgroundColor = .gray100
+        clipsToBounds = true
+    }
+    
+    func configure(with viewModel: RandomSubjectViewModel) {
+        contentLabel.text = viewModel.contentText
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentLabel.sizeToFit()
+        questionLabel.frame = CGRect(x: (UIScreen.main.bounds.width - 315) / 2,
+                                     y: 16,
+                                     width: 14,
+                                     height: 17)
+        contentLabel.frame = CGRect(x: (UIScreen.main.bounds.width - 315) / 2,
+                                    y: 14,
+                                    width: 315,
+                                    height: 42)
+        refreshButton.frame = CGRect(x: (UIScreen.main.bounds.width - 315) / 2 + 315 - 22,
+                                     y: 14 + contentLabel.frame.size.height + 3,
+                                     width: 22,
+                                     height: 22)
+    }
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setAttribute()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
--- Smeme-iOS/Smeme-iOS/Sources/Screen/MyDiary/MyDiaryViewController.swift
+++ Smeme-iOS/Smeme-iOS/Sources/Screen/MyDiary/MyDiaryViewController.swift
@@ -13,14 +13,25 @@ final class MyDiaryViewController: UIViewController {
     
     // MARK: - UI Property
     
+    var tmpView = RandomSubjectView(frame: CGRect(x: 0, y: 120, width: UIScreen.main.bounds.width, height: 91))
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        setAttribute()
     }
     
     // MARK: - @objc
     
     // MARK: - Custom Method
-
+    func setAttribute() {
+        view.addSubviews([tmpView])
+        view.backgroundColor = .white
+        
+        tmpView = tmpView.then {
+            $0.configure(with: RandomSubjectViewModel(contentText: "     " + "오늘부터 딱 일주일 후! 설레는 크리스마스네요. 일주일 전부터 세워보는 나의 크리스마스 계획은?"))
+        }
+    }
 }
