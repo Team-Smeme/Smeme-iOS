4020c683a4e638197d9f8a4d0bcd786aacabc847
임주민 <limj1213@gmail.com>
2023-01-07 00:13:35 +0900

    [Feat] #9 - UIView 노치관련 익스텐션 추가

--- Smeme-iOS/Smeme-iOS/Resources/Extension/UIView+.swift
+++ Smeme-iOS/Smeme-iOS/Resources/Extension/UIView+.swift
@@ -11,4 +11,9 @@ extension UIView {
     func addSubviews(_ views: [UIView]) {
         views.forEach { self.addSubview($0) }
     }
+    
+    ///Constraint 설정 시 노치 유무로 기기를 대응하는 상황에서 사용
+    func constraintByNotch(_ hasNotch: CGFloat, _ noNotch: CGFloat) -> CGFloat {
+        return UIScreen.main.hasNotch ? hasNotch : noNotch
+    }
 }
