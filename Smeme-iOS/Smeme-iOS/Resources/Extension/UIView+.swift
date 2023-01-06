75525d2c9be35c2c18187d3f1654feecf61b0064
cchanmi <gkfn980@hs.ac.kr>
2023-01-06 17:02:03 +0900

    [Feat] #27 - UIView cornerRadius 메서드 구현

--- Smeme-iOS/Smeme-iOS/Resources/Extension/UIView+.swift
+++ Smeme-iOS/Smeme-iOS/Resources/Extension/UIView+.swift
@@ -11,4 +11,9 @@ extension UIView {
     func addSubviews(_ views: [UIView]) {
         views.forEach { self.addSubview($0) }
     }
+    
+    func roundCorners(cornerRadius: CGFloat, maskedCorners: CACornerMask) {
+        layer.cornerRadius = cornerRadius
+        layer.maskedCorners = CACornerMask(arrayLiteral: maskedCorners)
+    }
 }
