ec0b43b09f8a118ee119cbf306fa2d98e5f402b2
cchanmi <gkfn980@hs.ac.kr>
2023-01-07 01:06:02 +0900

    [Chore] #27 - open diary collectionView cell에 final 추가

--- Smeme-iOS/Smeme-iOS/Sources/Screen/OpenDiary/OpenDiaryCollectionViewCell.swift
+++ Smeme-iOS/Smeme-iOS/Sources/Screen/OpenDiary/OpenDiaryCollectionViewCell.swift
@@ -10,7 +10,7 @@ import UIKit
 import SnapKit
 import Then
 
-class OpenDiaryCollectionViewCell: UICollectionViewCell {
+final class OpenDiaryCollectionViewCell: UICollectionViewCell {
     
     // MARK: - UI Property
     
