62e596a3942988aa3aea2840ce3fd6a70019bdfc
cchanmi <gkfn980@hs.ac.kr>
2023-01-07 01:18:11 +0900

    [Feat] #27 - open diary collectionView cell identifier

--- Smeme-iOS/Smeme-iOS/Sources/Screen/OpenDiary/OpenDiaryCollectionViewCell.swift
+++ Smeme-iOS/Smeme-iOS/Sources/Screen/OpenDiary/OpenDiaryCollectionViewCell.swift
@@ -12,6 +12,8 @@ import Then
 
 final class OpenDiaryCollectionViewCell: UICollectionViewCell {
     
+    static let identifier = "OpenDiaryCollectionViewCell"
+    
     // MARK: - UI Property
     
     private let categoryLabel = UILabel()
