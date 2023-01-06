cdb8b98362386f428f2f76566240b37f235e9826
cchanmi <gkfn980@hs.ac.kr>
2023-01-06 17:48:43 +0900

    [Feat] #27 - header label, button

--- Smeme-iOS/Smeme-iOS/Sources/Screen/OpenDiary/OpenDiaryViewController.swift
+++ Smeme-iOS/Smeme-iOS/Sources/Screen/OpenDiary/OpenDiaryViewController.swift
@@ -21,6 +21,22 @@ final class OpenDiaryViewController: UIViewController {
         $0.roundCorners(cornerRadius: 30, maskedCorners: [.layerMinXMaxYCorner, .layerMaxXMaxYCorner])
     }
     
+    private let containerView = UIView().then {
+        $0.backgroundColor = .clear
+    }
+    
+    private let titleLable = UILabel().then {
+        $0.text = "둘러보기"
+        $0.font = .headline3
+        $0.textColor = .black
+        $0.setTextWithLineHeight(lineHeight: 26)
+    }
+    
+    private lazy var myProfileButon = UIButton().then {
+        $0.setImage(Constant.Image.icnProfile, for: .normal)
+//        $0.addTarget(self, action: #selector(myProfileButtonDidTap), for: .touchUpInside)
+    }
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
@@ -39,13 +55,30 @@ final class OpenDiaryViewController: UIViewController {
     }
     
     private func setLayout() {
-        view.addSubview(headerView)
+        view.addSubviews([headerView])
+        headerView.addSubview(containerView)
+        containerView.addSubviews([titleLable, myProfileButon])
         
         headerView.snp.makeConstraints {
             $0.top.leading.trailing.equalToSuperview()
             $0.height.equalTo(198)
         }
         
+        containerView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(66)
+        }
+        
+        titleLable.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(30)
+        }
+        
+        myProfileButon.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(18)
+        }
     }
 
 }
