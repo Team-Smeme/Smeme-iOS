3db92dd06a9f49eba006c553524af80578297c43
임주민 <limj1213@gmail.com>
2023-01-06 17:45:09 +0900

    [Chore] #9 - DesignSystem 코드 컨벤션 맞춰서 수정

--- Smeme-iOS/Smeme-iOS/Sources/Screen/MyDiary/MyDiaryViewController.swift
+++ Smeme-iOS/Smeme-iOS/Sources/Screen/MyDiary/MyDiaryViewController.swift
@@ -21,6 +21,7 @@ final class MyDiaryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     
+        setBackgroundColor()
         setAttribute()
         setLayout()
     }
@@ -29,7 +30,13 @@ final class MyDiaryViewController: UIViewController {
     
     // MARK: - Custom Method
     
+    private func setBackgroundColor() {
+        view.backgroundColor = .white
+    }
+    
     private func setLayout() {
+        view.addSubviews([withRefreshView, noRefreshView])
+        
         withRefreshView.snp.makeConstraints {
             $0.top.equalToSuperview().inset(163)
             $0.leading.equalTo(self.view.snp.leading)
@@ -42,9 +49,6 @@ final class MyDiaryViewController: UIViewController {
     }
     
     private func setAttribute() {
-        view.addSubviews([withRefreshView, noRefreshView])
-        view.backgroundColor = .white
-        
         withRefreshView = withRefreshView.then {
             $0.configure(with: RandomSubjectViewModel(contentText: "오늘부터 딱 일주일 후! 설레는 크리스마스네요. 일주일 전부터 세워보는 나의 크리스마스 계획은? 오늘부터 딱 일주일 후! 설레는 크리스마스네요. 일주일 전부터 세워보는 나의 크리스마스 계획은?", isHiddenRefreshButton: false))
             $0.refreshSubjectContent = {
