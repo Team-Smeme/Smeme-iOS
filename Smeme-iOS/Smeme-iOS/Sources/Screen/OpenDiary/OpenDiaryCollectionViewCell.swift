2476c247942086e0e9775dc31e57051bec7588d2
cchanmi <gkfn980@hs.ac.kr>
2023-01-07 01:04:24 +0900

    [Feat] #27 - open diary collectionView cell layout

---
+++ Smeme-iOS/Smeme-iOS/Sources/Screen/OpenDiary/OpenDiaryCollectionViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  OpenDiaryCollectionViewCell.swift
+//  Smeme-iOS
+//
+//  Created by 황찬미 on 2023/01/06.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class OpenDiaryCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Property
+    
+    // MARK: - UI Property
+    
+    private let categoryLabel = UILabel()
+    
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - @objc
+    
+    // MARK: - Custom Method
+    
+    private func setLayout() {
+        addSubview(categoryLabel)
+        
+        categoryLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+}
