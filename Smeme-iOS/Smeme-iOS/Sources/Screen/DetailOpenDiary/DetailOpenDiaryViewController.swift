c2170e0913d0094711f0eda0ab55c9f04f6403a1
cchanmi <gkfn980@hs.ac.kr>
2023-01-08 20:02:52 +0900

    [Feat] #53 - create detail open diary

---
+++ Smeme-iOS/Smeme-iOS/Sources/Screen/DetailOpenDiary/DetailOpenDiaryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailOpenDiaryViewController.swift
+//  Smeme-iOS
+//
+//  Created by 황찬미 on 2023/01/08.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class DetailOpenDiaryViewController: UIViewController {
+    
+    // MARK: - Property
+    
+    // MARK: - UI Property
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - @objc
+    
+    // MARK: - Custom Method
+
+}
