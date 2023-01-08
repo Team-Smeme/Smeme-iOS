f5e8259d08d5ed4db44ac4e2789a1d55b28fabb1
임주민 <limj1213@gmail.com>
2023-01-09 07:09:47 +0900

    [Feat] #45 - 스크롤뷰 내용 UI 구현

--- Smeme-iOS/Smeme-iOS/Sources/Screen/MyDiary/DetailMyDiaryViewController.swift
+++ Smeme-iOS/Smeme-iOS/Sources/Screen/MyDiary/DetailMyDiaryViewController.swift
@@ -16,13 +16,7 @@ final class DetailMyDiaryViewController: UIViewController {
     // MARK: - UI Property
     
     private let contentView = UIView()
-    
-    private let contentScrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.showsVerticalScrollIndicator = false
-        
-        return scrollView
-    }()
+    private let contentScrollView = UIScrollView()
     
     private lazy var beforeButton = UIButton().then {
         $0.setImage(Constant.Image.icnPageLeft, for: .normal)
@@ -45,11 +39,12 @@ final class DetailMyDiaryViewController: UIViewController {
         $0.textColor = .gray500
         $0.font = .body2
         $0.setTextWithLineHeight(lineHeight: 17)
-        $0.text = "비공개"
+        $0.text = "공개"
     }
     
     private let categoryBackgroundView = UIView().then {
         $0.backgroundColor = .primary
+        $0.sizeToFit()
         $0.makeRoundCorner(cornerRadius: 10)
     }
     
@@ -72,6 +67,8 @@ final class DetailMyDiaryViewController: UIViewController {
         super.viewDidLoad()
         
         setBackgroundColor()
+        setData()
+        setTabbarHidden()
         setLayout()
     }
     
@@ -81,27 +78,81 @@ final class DetailMyDiaryViewController: UIViewController {
         view.backgroundColor = .smemeWhite
     }
     
-    private func setLayout() {
-        view.addSubviews([contentScrollView, beforeButton, dotsButton])
-        contentScrollView.addSubview(contentView)
-        contentView.addSubviews([categoryBackgroundView, categoryLabel, contentLabel, dateLabel])
-        
-        contentScrollView.snp.makeConstraints {
-            $0.top.equalToSuperview().inset(headerHeightByNotch(66))
-            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
-            $0.bottom.equalToSuperview().inset(bottomHeightByNotch(54))
-            $0.width.equalTo(UIScreen.main.bounds.width)
-        }
-        
-        contentView.snp.makeConstraints {
-            $0.top.leading.bottom.trailing.equalTo(contentScrollView)
-            $0.width.equalTo(contentScrollView)
-        }
+    private func setTabbarHidden() {
+        self.tabBarController?.tabBar.isHidden = true
     }
     
     private func setData() {
+        publicLabel.isHidden = !myDiaryDetail.isPublic
         categoryLabel.text = myDiaryDetail.category
-        contentLabel.text = myDiaryDetail.content
+        contentLabel.text = myDiaryDetail.content + " (\(myDiaryDetail.content.count))"
+        contentLabel.setTextWithLineHeight(lineHeight: 21)
         dateLabel.text = myDiaryDetail.createdAt.getFormattedDate(format: "yyyy년 MM월 dd일 HH:mm")
+    }
+    
+    private func setLayout() {
+        view.addSubviews([contentScrollView, beforeButton, dotsButton])
+        contentScrollView.addSubview(contentView)
+        contentView.addSubviews([categoryBackgroundView, categoryLabel, publicLabel, contentLabel, dateLabel])
+        
+        contentScrollView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(headerHeightByNotch(66))
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(bottomHeightByNotch(54))
+        }
+        
+        contentView.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalTo(contentScrollView)
+            $0.width.equalTo(contentScrollView.snp.width)
+            $0.height.greaterThanOrEqualTo(view.snp.height).priority(.low)
+        }
+        
+        beforeButton.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(headerHeightByNotch(10))
+            $0.leading.equalToSuperview().inset(convertByWidthRatio(18))
+        }
+        
+        dotsButton.snp.makeConstraints {
+            $0.centerY.equalTo(beforeButton)
+            $0.trailing.equalToSuperview().inset(convertByWidthRatio(29))
+        }
+    
+        categoryBackgroundView.snp.makeConstraints {
+            $0.top.equalTo(contentView).inset(convertByHeightRatio(15))
+            $0.leading.equalTo(contentView).inset(convertByWidthRatio(30))
+            $0.height.equalTo(29)
+            $0.width.equalTo(categoryLabel.intrinsicContentSize.width + 16)
+        }
+        
+        categoryLabel.snp.makeConstraints {
+            $0.center.equalTo(categoryBackgroundView)
+        }
+        
+        publicLabel.snp.makeConstraints {
+            $0.centerY.equalTo(categoryBackgroundView)
+            $0.trailing.equalTo(contentView).inset(convertByWidthRatio(30))
+        }
+        
+        contentLabel.snp.makeConstraints {
+            $0.top.equalTo(categoryBackgroundView.snp.bottom).offset(convertByHeightRatio(20))
+            $0.leading.equalTo(contentView).inset(convertByWidthRatio(30))
+            $0.trailing.equalTo(contentView).inset(convertByWidthRatio(30))
+        }
+        
+        dateLabel.snp.makeConstraints {
+            $0.top.equalTo(contentLabel.snp.bottom).offset(convertByHeightRatio(20))
+            $0.trailing.equalTo(contentView).inset(convertByWidthRatio(30))
+        }
+    }
+    
+    // MARK: - objc
+    
+    @objc func touchBeforeButton(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+        self.tabBarController?.tabBar.isHidden = false
+    }
+    
+    @objc func touchDotsButton(_ sender: UIButton) {
+        
     }
 }
