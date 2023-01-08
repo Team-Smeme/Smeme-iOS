2a8eb9b351cf76d35d7f4784fefcd3fcb674af36
임주민 <limj1213@gmail.com>
2023-01-09 02:07:39 +0900

    [Feat] #45 - 내일기상세뷰 추가

---
+++ Smeme-iOS/Smeme-iOS/Sources/Screen/MyDiary/DetailMyDiaryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailMyDiaryViewController.swift
+//  Smeme-iOS
+//
+//  Created by 임주민 on 2023/01/09.
+//
+
+import UIKit
+
+class DetailMyDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
