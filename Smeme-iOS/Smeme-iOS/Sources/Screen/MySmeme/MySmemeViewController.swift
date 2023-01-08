2b4895006f1a32bbb1687b43a26529b72141c6e0
Jonghwa Won <jongwha95@gmail.com>
2023-01-09 04:59:49 +0900

    [Feat] #34 - 컨테이너 뷰 레이아웃 구현, divideLine1 레이아웃 구현

--- Smeme-iOS/Smeme-iOS/Sources/Screen/MySmeme/MySmemeViewController.swift
+++ Smeme-iOS/Smeme-iOS/Sources/Screen/MySmeme/MySmemeViewController.swift
@@ -19,7 +19,10 @@ final class MySmemeViewController: UIViewController {
     private let headerContainerView = UIView()
     private let setUserInfoContainerView = UIView()
     private let setMainLanguageContainerView = UIView()
-    private let divideLine1 = UIView()
+    
+    private let divideLine1 = UIView().then {
+        $0.backgroundColor = .gray100
+    }
     
     private let previousButton = UIButton().then {
         $0.setImage(Constant.Image.icnPageLeft, for: .normal)
@@ -47,10 +50,30 @@ final class MySmemeViewController: UIViewController {
             $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
             $0.height.equalTo(convertByHeightRatio(66))
         }
+        
+        setUserInfoContainerView.snp.makeConstraints {
+            $0.top.equalTo(headerContainerView.snp.bottom)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(convertByHeightRatio(83))
+        }
+        
+        setMainLanguageContainerView.snp.makeConstraints {
+            $0.top.equalTo(setUserInfoContainerView.snp.bottom).offset(12)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(convertByHeightRatio(47))
+        }
+        
+        divideLine1.snp.makeConstraints {
+            $0.top.equalTo(setMainLanguageContainerView.snp.bottom).offset(14)
+            $0.height.equalTo(1)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(18)
+        }
     }
     
     private func setBackgroundColor() {
         view.backgroundColor = .smemeWhite
         headerContainerView.backgroundColor = .blue
+        setUserInfoContainerView.backgroundColor = .red
+        setMainLanguageContainerView.backgroundColor = .gray
     }
 }
