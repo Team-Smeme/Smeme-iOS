64a825ba23fce649fc5c5f39ca474a5dd790a56f
cchanmi <gkfn980@hs.ac.kr>
2023-01-08 22:51:11 +0900

    [Feat] #53 - headerView 구현

--- Smeme-iOS/Smeme-iOS/Sources/Screen/DetailOpenDiary/DetailOpenDiaryViewController.swift
+++ Smeme-iOS/Smeme-iOS/Sources/Screen/DetailOpenDiary/DetailOpenDiaryViewController.swift
@@ -16,14 +16,42 @@ final class DetailOpenDiaryViewController: UIViewController {
     
     // MARK: - UI Property
     
+    private let headerView = UIView()
+    
+    private let backButton = UIButton().then {
+        $0.setImage(Constant.Image.icnPageLeft, for: .normal)
+    }
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setBackgroundColor()
+        setLayout()
     }
     
     // MARK: - @objc
     
     // MARK: - Custom Method
+    
+    private func setBackgroundColor() {
+        view.backgroundColor = .white
+    }
+    
+    private func setLayout() {
+        view.addSubviews([headerView])
+        headerView.addSubview(backButton)
+        
+        headerView.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(66)
+        }
+        
+        backButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(28)
+        }
+    }
 
 }
