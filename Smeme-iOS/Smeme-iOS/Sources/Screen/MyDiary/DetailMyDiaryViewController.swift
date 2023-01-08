867cf1ba160989c234d6cd49c3bca575ca32c6f3
임주민 <limj1213@gmail.com>
2023-01-09 02:09:17 +0900

    [Feat] #45 - ScrollView 추가

--- Smeme-iOS/Smeme-iOS/Sources/Screen/MyDiary/DetailMyDiaryViewController.swift
+++ Smeme-iOS/Smeme-iOS/Sources/Screen/MyDiary/DetailMyDiaryViewController.swift
@@ -2,28 +2,56 @@
 //  DetailMyDiaryViewController.swift
 //  Smeme-iOS
 //
-//  Created by 임주민 on 2023/01/09.
+//  Created by 임주민 on 2023/01/08.
 //
 
 import UIKit
 
-class DetailMyDiaryViewController: UIViewController {
-
+final class DetailMyDiaryViewController: UIViewController {
+    
+    // MARK: - Property
+    
+    // MARK: - UI Property
+    
+    private let contentView = UIView()
+    
+    private let contentScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.showsVerticalScrollIndicator = false
+        
+        return scrollView
+    }()
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setBackgroundColor()
+        setLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Custom Method
+    
+    private func setBackgroundColor() {
+        view.backgroundColor = .smemeWhite
     }
-    */
-
+    
+    private func setLayout() {
+        view.addSubviews([contentScrollView, beforeButton, dotsButton])
+        contentScrollView.addSubview(contentView)
+        contentView.addSubviews([categoryBackgroundView, categoryLabel, contentLabel, dateLabel])
+        
+        contentScrollView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(headerHeightByNotch(66))
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.bottom.equalToSuperview().inset(bottomHeightByNotch(54))
+            $0.width.equalTo(UIScreen.main.bounds.width)
+        }
+        
+        contentView.snp.makeConstraints {
+            $0.top.leading.bottom.trailing.equalTo(contentScrollView)
+            $0.width.equalTo(contentScrollView)
+        }
+    }
 }
