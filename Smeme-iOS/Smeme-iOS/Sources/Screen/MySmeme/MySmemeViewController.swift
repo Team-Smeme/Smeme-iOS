b418985c43c7103f1216fe828c8495c34c9bdfd1
Jonghwa Won <jongwha95@gmail.com>
2023-01-09 04:01:49 +0900

    [Feat] #34 - 마이스밈 뷰 VC 생성

---
+++ Smeme-iOS/Smeme-iOS/Sources/Screen/MySmeme/MySmemeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MySmemeViewController.swift
+//  Smeme-iOS
+//
+//  Created by JH on 2023/01/09.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class MySmemeViewController: UIViewController {
+    
+    // MARK: - Property
+    
+    // MARK: - UI Property
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    // MARK: - @objc
+    
+    // MARK: - Custom Method
+    
+}
