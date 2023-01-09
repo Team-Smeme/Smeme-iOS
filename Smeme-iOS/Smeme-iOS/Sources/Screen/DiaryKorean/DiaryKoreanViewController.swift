897fe6e7ee487459eeba7ec0bb4c4dfea8bb932d
Joon Baek <joonbaek12@gmail.com>
2023-01-09 11:28:52 +0900

    [Feat] #68 한국어 일기부터 시작 NaviView 컴포넌트 생성

---
+++ Smeme-iOS/Smeme-iOS/Sources/Screen/DiaryKorean/DiaryKoreanViewController.swift
@@ -0,0 +1,56 @@
+//
+//  DiaryKoreanViewController.swift
+//  Smeme-iOS
+//
+//  Created by Joon Baek on 2023/01/09.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class DiaryKoreanViewController: UIViewController {
+    // MARK: - Property
+    // MARK: - UI Property
+    
+    private let naviView = UIView()
+    private let languageView = UIView()
+    
+    private let cancelButton = UIButton().then {
+        $0.titleLabel?.font = .body1
+        $0.setTitleColor(.black, for: .normal)
+        $0.setTitle("취소", for: .normal)
+    }
+    
+    private let completeButton = UIButton().then {
+        $0.titleLabel?.font = .body1
+        $0.setTitleColor(.black, for: .normal)
+        $0.setTitle("완료", for: .normal)
+    }
+    
+    private let languageLabel = UILabel().then {
+        $0.font = .subtitle2
+        $0.textColor = .gray500
+        $0.text = "English"
+    }
+    
+    private let languageIcon = UIImageView(image: Constant.Image.icnArrowUnder)
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setBackgoundColor()
+        setLayout()
+
+    }
+    
+    // MARK: - @objc
+    
+    // MARK: - Custom Method
+    private func setBackgoundColor() {
+        view.backgroundColor = .smemeWhite
+    }
+}
