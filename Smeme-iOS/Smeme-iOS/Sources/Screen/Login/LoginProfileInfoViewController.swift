134be33fcd72ae5993cda4dcd0e1ec9074dd041e
임주민 <limj1213@gmail.com>
2023-01-09 18:04:53 +0900

    [Add] #67 - 프로필 입력하는 뷰 추가

---
+++ Smeme-iOS/Smeme-iOS/Sources/Screen/Login/LoginProfileInfoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginProfileInfoViewController.swift
+//  Smeme-iOS
+//
+//  Created by 임주민 on 2023/01/09.
+//
+
+import UIKit
+
+class LoginProfileInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
