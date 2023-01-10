8372974f4de7d7fdb21ddbbf38d699647e9e31ad
cchanmi <gkfn980@hs.ac.kr>
2023-01-11 06:17:14 +0900

    [Feat] #107 - OpenDiaryCategoryService

---
+++ Smeme-iOS/Smeme-iOS/Sources/Network/Service/OpenDiaryCategoryService.swift
@@ -0,0 +1,39 @@
+//
+//  OpenDiaryCategoryService.swift
+//  Smeme-iOS
+//
+//  Created by 황찬미 on 2023/01/11.
+//
+
+import Moya
+
+enum OpenDiaryCategoryService {
+    case openDiaryCategory
+}
+
+extension OpenDiaryCategoryService: BaseTargetType {
+    var path: String {
+        switch self {
+        case .openDiaryCategory:
+            return URLConstant.openDiaryCategoryURL
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .openDiaryCategory:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .openDiaryCategory:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        return NetworkConstant.hasTokenHeader
+    }
+}
