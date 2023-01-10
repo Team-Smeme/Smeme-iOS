25b8994a0054fcf35608ada3900e872f6a20f6b6
cchanmi <gkfn980@hs.ac.kr>
2023-01-10 22:28:05 +0900

    [Chore] #86 - TextView 메서드 주석 수정

--- Smeme-iOS/Smeme-iOS/Resources/Extension/UITextView+.swift
+++ Smeme-iOS/Smeme-iOS/Resources/Extension/UITextView+.swift
@@ -8,13 +8,16 @@
 import UIKit
 
 extension UITextView {
-    /// 레이아웃 잡을 때 값 24로 주기 ex) $0.leading.trailing.equalToSuperview().inset(24)
+    /*
+     1. textColor는 뷰컨에서 해당 textView 속성 정의할 때 정의해 주어야 함!
+     2. 레이아웃 잡을 때 값 24로 주기 ex) $0.leading.trailing.equalToSuperview().inset(24)
+     3. textView 위에 label 올릴 때의 레이아웃 ex) top.leading.equalToSuperview().inset(7)
+     */
     func setLineSpacing() {
         let style = NSMutableParagraphStyle()
         style.lineSpacing = 2
         let attributes = [
             NSAttributedString.Key.paragraphStyle: style,
-            NSAttributedString.Key.foregroundColor: UIColor.black,
             NSAttributedString.Key.font: UIFont.body1
         ]
         self.textAlignment = .left
