d17266509a0968e30e2bfd8c59f88b621463d80e
임주민 <limj1213@gmail.com>
2023-01-11 07:36:45 +0900

    [Feat] #110 - MyDiaryService 추가

---
+++ Smeme-iOS/Smeme-iOS/Sources/Network/Service/MyDiaryService.swift
@@ -0,0 +1,43 @@
+//
+//  MyDiaryService.swift
+//  Smeme-iOS
+//
+//  Created by 임주민 on 2023/01/11.
+//
+
+import Moya
+
+enum MyDiaryService {
+    case totalMyDiary
+    // case detailMydiary(diaryId: Int)
+}
+
+extension MyDiaryService: BaseTargetType {
+    var path: String {
+        switch self {
+        case .totalMyDiary:
+            return "/users/diaries"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .totalMyDiary:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .totalMyDiary:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String: String]? {
+        switch self {
+        case .totalMyDiary:
+            return NetworkConstant.hasTokenHeader
+        }
+    }
+}
