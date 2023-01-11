843039914bf38859ab22628c7dc35a584f803a25
Joon Baek <joonbaek12@gmail.com>
2023-01-11 23:43:59 +0900

    [Chore] #122 - PostDiaryService 작성

--- Smeme-iOS/Smeme-iOS/Sources/Network/Service/PostDiary/PostDiaryService.swift
+++ Smeme-iOS/Smeme-iOS/Sources/Network/Service/PostDiary/PostDiaryService.swift
@@ -1,8 +1,39 @@
 //
-//  ForeignDiaryService.swift
+//  PostDiaryService.swift
 //  Smeme-iOS
 //
 //  Created by Joon Baek on 2023/01/11.
 //
 
-import Foundation
+import Moya
+
+enum PostDiaryService {
+    case postDiary
+}
+
+extension PostDiaryService: BaseTargetType {
+    var path: String {
+        switch self {
+        case .postDiary:
+            return URLConstant.postDiaryURL
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .postDiary:
+            return .post
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .postDiary:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String: String]? {
+        return NetworkConstant.hasTokenHeader
+    }
+}
