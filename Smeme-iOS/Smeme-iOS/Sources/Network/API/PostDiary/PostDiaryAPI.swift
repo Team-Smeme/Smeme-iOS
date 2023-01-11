61ecc499aea81557c00ffadf330d24563bb3ca9a
Joon Baek <joonbaek12@gmail.com>
2023-01-11 23:42:25 +0900

    [Chore] #122 - PostDiaryAPI 코드작성

--- Smeme-iOS/Smeme-iOS/Sources/Network/API/PostDiary/PostDiaryAPI.swift
+++ Smeme-iOS/Smeme-iOS/Sources/Network/API/PostDiary/PostDiaryAPI.swift
@@ -1,16 +1,35 @@
 //
-//  ForeignDiaryAPI.swift
+//  PostDiaryAPI.swift
 //  Smeme-iOS
 //
 //  Created by Joon Baek on 2023/01/11.
 //
 
-import Foundation
-
 import Moya
 
-final class ForeignDiaryAPI {
+final class PostDiaryAPI {
+    static let shared = postDiaryAPI = PostDiaryAPI()
+    private let postDiaryProvider = MoyaProvider<PostDiaryService>
+    (plugins: [MoyaLoggingPlugin])
     
-    static let shared = ForeignDiaryAPI()
+    private var postDiaryData: GeneralResponse<PostDiaryResponse>?
     
+    func postDiary(completion: @escaping
+                   (GeneralResponse<PostDiaryResponse>?) -> Void) {
+        postDiaryProvider.request(.postDiary) { response in
+            switch response {
+            case .success(let result):
+                do {
+                    self.postDiaryData = try
+                    result.map(GeneralResponse<PostDiaryResponse>
+                        .self)
+                    complention(self.postDiaryData)
+                } catch {
+                    print(error)
+                }
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
 }
