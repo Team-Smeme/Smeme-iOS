b81fbd0993fa797e55b42c8c32ccee8d3c83356f
Joon Baek <joonbaek12@gmail.com>
2023-01-11 22:53:47 +0900

    [Feat] #122 - PostDiary API, Service 파일 생성

---
+++ Smeme-iOS/Smeme-iOS/Sources/Network/API/PostDiary/PostDiaryAPI.swift
@@ -0,0 +1,16 @@
+//
+//  ForeignDiaryAPI.swift
+//  Smeme-iOS
+//
+//  Created by Joon Baek on 2023/01/11.
+//
+
+import Foundation
+
+import Moya
+
+final class ForeignDiaryAPI {
+    
+    static let shared = ForeignDiaryAPI()
+    
+}
---
+++ Smeme-iOS/Smeme-iOS/Sources/Network/Service/PostDiary/PostDiaryService.swift
@@ -0,0 +1,8 @@
+//
+//  ForeignDiaryService.swift
+//  Smeme-iOS
+//
+//  Created by Joon Baek on 2023/01/11.
+//
+
+import Foundation
