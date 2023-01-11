d558a1db6434c30e7283f84fc13e49aaac2ab06e
cchanmi <gkfn980@hs.ac.kr>
2023-01-12 03:38:15 +0900

    [Conflict] #131 - 충돌 해결

--- Smeme-iOS/Smeme-iOS/Sources/Network/Service/OpenDiary/OpenDiaryListService.swift
+++ Smeme-iOS/Smeme-iOS/Sources/Network/Service/OpenDiary/OpenDiaryListService.swift
@@ -9,32 +9,37 @@ import Moya
 
 enum OpenDiaryListService {
     case openDiaryTotalList
+    case openDiaryDailyList
+    case openDiarySelectList(param: Int)
 }
 
 extension OpenDiaryListService: BaseTargetType {
     var path: String {
         switch self {
         case .openDiaryTotalList:
-            return URLConstant.openDiaryToTalListURL
+            return URLConstant.openDiaryTotalListURL
+        case .openDiaryDailyList:
+            return URLConstant.openDiarySelectListURL + "0"
+        case .openDiarySelectList(let param):
+            return URLConstant.openDiarySelectListURL + "\(param)"
         }
     }
 
     var method: Moya.Method {
         switch self {
-        case .openDiaryTotalList:
+        case .openDiaryTotalList, .openDiaryDailyList, .openDiarySelectList:
             return .get
         }
     }
 
     var task: Moya.Task {
         switch self {
-        case .openDiaryTotalList:
+        case .openDiaryTotalList, .openDiaryDailyList, .openDiarySelectList:
             return .requestPlain
         }
     }
 
-    var headers: [String : String]? {
+    var headers: [String: String]? {
         return NetworkConstant.hasTokenHeader
     }
 }
-
