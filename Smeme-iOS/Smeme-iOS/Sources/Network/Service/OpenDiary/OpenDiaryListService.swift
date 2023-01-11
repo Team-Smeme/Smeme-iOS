05f2502a734432649c471cf97d7a1e21b13b7fa5
cchanmi <gkfn980@hs.ac.kr>
2023-01-12 07:15:20 +0900

    [Chore] #137 - query param service 작성

--- Smeme-iOS/Smeme-iOS/Sources/Network/Service/OpenDiary/OpenDiaryListService.swift
+++ Smeme-iOS/Smeme-iOS/Sources/Network/Service/OpenDiary/OpenDiaryListService.swift
@@ -9,33 +9,30 @@ import Moya
 
 enum OpenDiaryListService {
     case openDiaryTotalList
-    case openDiaryDailyList
     case openDiarySelectList(param: Int)
 }
 
 extension OpenDiaryListService: BaseTargetType {
     var path: String {
         switch self {
-        case .openDiaryTotalList:
+        case .openDiaryTotalList, .openDiarySelectList:
             return URLConstant.openDiaryTotalListURL
-        case .openDiaryDailyList:
-            return URLConstant.openDiarySelectListURL + "0"
-        case .openDiarySelectList(let param):
-            return URLConstant.openDiarySelectListURL + "\(param)"
         }
     }
 
     var method: Moya.Method {
         switch self {
-        case .openDiaryTotalList, .openDiaryDailyList, .openDiarySelectList:
+        case .openDiaryTotalList, .openDiarySelectList:
             return .get
         }
     }
 
     var task: Moya.Task {
         switch self {
-        case .openDiaryTotalList, .openDiaryDailyList, .openDiarySelectList:
+        case .openDiaryTotalList:
             return .requestPlain
+        case .openDiarySelectList(let param):
+            return .requestParameters(parameters: ["category": param], encoding: URLEncoding.queryString)
         }
     }
 
