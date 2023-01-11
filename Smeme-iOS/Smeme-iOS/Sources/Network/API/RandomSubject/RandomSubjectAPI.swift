e1d540e0d176612c75865908d78ef66b292cd498
Joon Baek <joonbaek12@gmail.com>
2023-01-11 23:14:56 +0900

    [Chore] #122 - RandomSubject API 작성

--- Smeme-iOS/Smeme-iOS/Sources/Network/API/RandomSubject/RandomSubjectAPI.swift
+++ Smeme-iOS/Smeme-iOS/Sources/Network/API/RandomSubject/RandomSubjectAPI.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+final class RandomSubjectAPI {
+    static let shared: RandomSubjectAPI = RandomSubjectAPI
+    private let randomSubjectProvider = MoyaProvider<RandomSubjectService>(plugins: [MoyaLoggingPlugin()])
+    
+    private var randomSubjectData: GeneralResponse<RandomSubjectResponse>?)
+    
+    func getRandomSubject(completion: @escaping
+                          (RandomSubjectResponse<OpenDiaryCategoryResponse>?) -> Void) {
+        randomSubjectProvider.request(.randomSubject) { response in
+            switch response {
+            case .success(let result):
+                do {
+                    self.randomSubjectData = try
+                    result.map(GeneralResponse<RandomSubjectResponse>.self)
+                    completion(self.randomSubjectData)
+                } catch {
+                    print(error)
+                }
+            case .failure(let err):
+                print(err)
+                
+            }
+        }
+    }
+}
