24c9790f3c8027a497a84069407e268fc5b3799a
Joon Baek <joonbaek12@gmail.com>
2023-01-11 22:50:20 +0900

    [Feat] #122 - RandomSubject API, Service 생성

---
+++ Smeme-iOS/Smeme-iOS/Sources/Network/API/RandomSubject/RandomSubjectAPI.swift
@@ -0,0 +1,8 @@
+//
+//  RandomSubjectAPI.swift
+//  Smeme-iOS
+//
+//  Created by Joon Baek on 2023/01/11.
+//
+
+import Foundation
---
+++ Smeme-iOS/Smeme-iOS/Sources/Network/Service/RandomSubject/RandomSubjectService.swift
@@ -0,0 +1,8 @@
+//
+//  RandomSubjectService.swift
+//  Smeme-iOS
+//
+//  Created by Joon Baek on 2023/01/11.
+//
+
+import Foundation
