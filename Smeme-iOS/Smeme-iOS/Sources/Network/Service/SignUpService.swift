09efd597277735ef9c47745f9f9666428e922d02
임주민 <limj1213@gmail.com>
2023-01-12 09:55:06 +0900

    [Feat] #140 - 회원가입 Service 파일 작성

---
+++ Smeme-iOS/Smeme-iOS/Sources/Network/Service/SignUpService.swift
@@ -0,0 +1,37 @@
+//
+//  SignUpService.swift
+//  Smeme-iOS
+//
+//  Created by 임주민 on 2023/01/12.
+//
+
+import Moya
+
+enum SignUpService {
+    case signUp(accessToken: String, username: String, bio: String)
+}
+
+extension SignUpService: BaseTargetType {
+    var path: String {
+        return URLConstant.signUpURL
+    }
+    
+    var method: Moya.Method {
+        return .patch
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .signUp(_, let username, let bio):
+            return .requestParameters(parameters: ["username": username, "bio": bio], encoding: URLEncoding.queryString)
+        }
+    }
+    
+    var headers: [String: String]? {
+        switch self {
+        case .signUp(let accessToken, _, _):
+            return ["Content-Type": "application/json",
+                    "Authorization": accessToken]
+        }
+    }
+}
