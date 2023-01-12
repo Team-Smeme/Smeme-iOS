cb33448a123d6eb1c40eb4dc0873322f0620d0e0
cchanmi <gkfn980@hs.ac.kr>
2023-01-12 09:40:38 +0900

    [Feat] #144 - Scrap open diary service 작성

---
+++ Smeme-iOS/Smeme-iOS/Sources/Network/Service/OpenDiary/ScrapOpenDirayService.swift
@@ -0,0 +1,39 @@
+//
+//  ScrapsOpenDirayService.swift
+//  Smeme-iOS
+//
+//  Created by 황찬미 on 2023/01/12.
+//
+
+import Moya
+
+enum ScrapOpenDirayService {
+    case scraps(param: ScrapRequest)
+}
+
+extension ScrapOpenDirayService: BaseTargetType {
+    var path: String {
+        switch self {
+        case .scraps:
+            return URLConstant.scrapOpenDirayURL
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .scraps:
+            return .post
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .scraps(let param):
+            return .requestJSONEncodable(param)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return NetworkConstant.hasTokenHeader
+    }
+}
