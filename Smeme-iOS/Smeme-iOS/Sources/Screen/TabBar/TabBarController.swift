772835e9bf177fc5c29c2bf398e2c93af3edcd25
Jonghwa Won <jongwha95@gmail.com>
2023-01-13 06:02:48 +0900

    [Feat] #157 - 그림자 적용 함수 생성

--- Smeme-iOS/Smeme-iOS/Sources/Screen/TabBar/TabBarController.swift
+++ Smeme-iOS/Smeme-iOS/Sources/Screen/TabBar/TabBarController.swift
@@ -56,6 +56,23 @@ extension TabBarController {
         tabBar.backgroundColor = .white
         tabBar.tintColor = .primary
     }
+// MARK: - SetShadow
+
+extension CALayer {
+    func applyShadow(
+        color: UIColor = .black,
+        alpha: Float = 0.5,
+        x: CGFloat = 0,
+        y: CGFloat = 2,
+        blur: CGFloat = 4
+    ) {
+        shadowColor = color.cgColor
+        shadowOpacity = alpha
+        shadowOffset = CGSize(width: x, height: y)
+        shadowRadius = blur / 2.0
+    }
+}
+
 extension UITabBar {
     static func clearShadow() {
         UITabBar.appearance().shadowImage = UIImage()
